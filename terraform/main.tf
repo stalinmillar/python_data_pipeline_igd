ff275b354bf0d763d2ed38fccb22141301fde8a9
stalinmillar <stalinmillar@gmail.com>
2025-04-08 01:17:27 +0530

    Update main.tf
    master load routine

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,6 +35,39 @@ resource "google_bigquery_table" "master" {
   }
 }
 
+resource "google_bigquery_routine" "load_to_master" {
+  dataset_id = google_bigquery_dataset.raw.dataset_id
+  routine_id = "load_to_master"
+  routine_type = "PROCEDURE"
+
+  definition_body = <<EOT
+  BEGIN
+    INSERT INTO `project_id.finance_raw.retail_macro_master_tb`
+    SELECT *,
+      CURRENT_TIMESTAMP() AS bq_write_time_timestamp,
+      CURRENT_TIMESTAMP() AS update_bq_timestamp,
+      TO_JSON_STRING(t) AS raw_payload,
+      CURRENT_DATE() AS date_partition_column
+    FROM `project_id.finance_raw.retail_macro_staging_tb` t;
+  END;
+  EOT
+}
+
+resource "google_bigquery_data_transfer_config" "scheduled_query" {
+  display_name           = "Scheduled Load to Master"
+  data_source_id         = "scheduled_query"
+  destination_dataset_id = google_bigquery_dataset.raw.dataset_id
+  params = {
+    query = <<EOT
+      CALL `project_id.finance_raw.load_to_master`();
+    EOT
+    destination_table_name_template = "ignore"
+    write_disposition               = "WRITE_APPEND"
+  }
+  schedule               = "every 24 hours"
+  location               = var.region
+}
+
 ===================Secret modules
 resource "google_secret_manager_secret" "sftp_username" {
   secret_id = "sftp_username"
