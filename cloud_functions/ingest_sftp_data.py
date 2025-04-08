9b3ae69fc2be328f5f2be64ca667da4ae7773b2a
stalinmillar <stalinmillar@gmail.com>
2025-04-08 15:07:36 +0530

    Create ingest_sftp_data.py

---
+++ cloud_functions/ingest_sftp_data.py
@@ -0,0 +1,30 @@
+import functions_framework
+import paramiko
+from google.cloud import storage
+from config.secrets_manager import get_secret
+
+@functions_framework.http
+def ingest_sftp_data(request):
+    hostname = get_secret("sftp_hostname")
+    username = get_secret("sftp_username")
+    password = get_secret("sftp_password")
+
+    sftp_path = "/path/to/monthly_data.json"
+
+    transport = paramiko.Transport((hostname, 22))
+    transport.connect(username=username, password=password)
+    sftp = paramiko.SFTPClient.from_transport(transport)
+
+    with sftp.file(sftp_path, 'r') as file:
+        data = file.read()
+        if len(data) < 1024:
+            return "File too small"
+
+        storage_client = storage.Client()
+        bucket = storage_client.bucket("your-staging-bucket")
+        blob = bucket.blob(os.path.basename(sftp_path))
+        blob.upload_from_string(data)
+
+    sftp.close()
+    transport.close()
+    return "SFTP file ingested"
